bdb482815f17ca59ad3e1fb69107a950a372028e
darrr <darrr2013@gmail.com>
2015-01-10 19:39:54 +0200

    initialised netbeans project

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: Dasha
+ *
+ * Created on January 10, 2015, 7:11 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "Hello world, how are you?";
+    return 0;
+}
+
